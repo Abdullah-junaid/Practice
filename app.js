87c676aecd2b9949ca014b330bfbf9e7d013133e
Abdullah-junaid <aj5388587@gmail.com>
2020-07-14 10:46:16 +0500

    Timer

---
+++ app.js
@@ -0,0 +1,24 @@
+/*var msec = document.getElementById('msec')
+var sec = document.getElementById('sec')
+var min = document.getElementById('min')
+var hour = document.getElementById('hour')
+var interval ;
+var caounter="0"
+
+function starter (){
+counter++
+console.log
+}
+
+function(interval) = setInterval(starter,1){}*/
+
+
+
+var msec = document.getElementById('msec')
+var sec = document.getElementById('sec')
+var min = document.getElementById('min')
+var hour = document.getElementById('hour')
+
+
+
+
