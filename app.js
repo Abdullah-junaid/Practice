1ddc68d5d76d141ae0e61885af179eab4ce98440
Abdullah-junaid <aj5388587@gmail.com>
2020-07-15 11:52:30 +0500

    first Stop Watch clear

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
-/*var msec = document.getElementById('msec')
-var sec = document.getElementById('sec')
-var min = document.getElementById('min')
-var hour = document.getElementById('hour')
-var interval ;
-var caounter="0"
 
-function starter (){
-counter++
-console.log
+var msecheading = document.getElementById('msec')
+var secheading = document.getElementById('sec')
+var minheading = document.getElementById('min')
+
+var msec = 0
+var sec = 0
+var min = 0
+Interval ;
+
+function timer (){
+    msec++
+    msecheading.innerHTML = msec
+    if(msec >= 99){
+        msec=0
+        sec++
+        secheading.innerHTML = sec 
+    }
+    else if(sec >=59){
+        sec=0
+        min++
+        minheading.innerHTML= min
+    }
 }
 
-function(interval) = setInterval(starter,1){}*/
+
+function start(){Interval = setInterval(timer,10)
+    startbtn.setAttribute('disabled','disabled')
+}
+
+function stop(){
+ clearInterval(Interval)
+ startbtn.removeAttribute('disabled')
+}
+
+function reset(){
+    minheading.innerHTML=0
+    secheading.innerHTML=0
+    msecheading.innerHTML=0
+    stop() 
+    
+  
+}
 
 
 
-var msec = document.getElementById('msec')
-var sec = document.getElementById('sec')
-var min = document.getElementById('min')
-var hour = document.getElementById('hour')
-
-
-
-var status = checkign
 
 
 
